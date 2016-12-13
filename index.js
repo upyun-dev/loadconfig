17f544b2266c58191b941f3120d07852c49a25f0
yu.zheng <yu.zheng@upai.com>
2016-12-13 10:53:39 +0800

    feat: 初始化，支持环境变量转换，配置文件读取

---
+++ index.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const deepExtend = require('deep-extend');
+const syncRequest = require('sync-request');
+const winston = require('winston');
+const loadEnv = require('./lib/load-env');
+
+const env = process.env;
+
+const loadConfig = (opts, callback) => {
+  opts = deepExtend({
+    pattern: 'config.%{env}.json',
+    url: '',
+    stages: [
+      'test',
+      'production',
+      'development'
+    ],
+    name: 'loadConfig'
+  }, opts || {});
+
+  // 后续修改
+  opts.pattern = './' + opts.pattern;
+
+  let cfg = require(opts.pattern.replace(/%{env}/, 'defaults'));
+  // 环境变量最好跟着 defaults 文件导入, 避免环境变量名被污染
+  let envCfg = loadEnv(cfg, { name: opts.name });
+
+  try {
+    cfg = deepExtend(cfg, require(opts.pattern.replace(/\.%{env}/, '')));
+  } catch (e) { }
+
+  // stage override
+  if (~opts.stages.indexOf(env.NODE_ENV)) {
+    let dir;
+    try {
+      dir = opts.pattern.replace(/%{env}/, env.NODE_ENV);
+      cfg = deepExtend(cfg, require(dir));
+    } catch (e) {
+      winston.log('info', dir, 'not a regular file');
+    }
+  }
+
+  if (opts.url) {
+    try {
+      let res = syncRequest('GET', opts.url);
+      let conf = JSON.parse(res.getBody('utf8'));
+      deepExtend(cfg, conf);
+    } catch (e) {
+      winston.log('info', `Request data from ${opts.url} is invalid`);
+      winston.log('error', e.message, { error: e.stack });
+    }
+  }
+  // Env override
+  deepExtend(cfg, envCfg);
+  return cfg;
+};
+
+exports.loadConfig = loadConfig;
+
---
+++ test/index.js
@@ -0,0 +1,9 @@
+const loadConfig = require('../index').loadConfig;
+
+describe('index.js', function() {
+  describe('#loadConfig()', function() {
+    it('', function() {
+      loadConfig();
+    });
+  });
+});
